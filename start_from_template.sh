ce4d29311ae8cc8dbdf334f9413f66744f588d8a
vstrobel volker.strobel87@gmail.com <vstrobel@compute-3-15.local>
2016-12-16 16:39:30 +0100

    included launcher

---
+++ start_from_template.sh
@@ -0,0 +1,23 @@
+TEMPLATE='experiments/epuck_EC_locale_template.argos'
+OUTFILE='experiments/epuck.argos'
+#BASEDIR='/home/volker/Downloads/code/code/argos_simulations/Epuck/controllers/'
+BASEDIR='/home/vstrobel/Documents/argdavide/controllers/epuck_environment_classification/'
+RADIX="volker_trial"
+NUMROBOTS=12
+R0=$(expr $NUMROBOTS / 2)
+B0=$(expr $NUMROBOTS / 2)
+PERCENT_RED=80
+PERCENT_BLUE=$(expr 100 - $PERCENT_RED)
+
+REPETITIONS=10
+
+for i in `seq 1 $REPETITIONS`; do
+
+    # Create template
+sed -e "s|BASEDIR|$BASEDIR|g" -e "s|RADIX|$RADIX_$i|g" -e "s|NUMROBOTS|$NUMROBOTS|g" -e "s|R0|$R0|g" -e "s|B0|$B0|g" -e "s|PERCENT_RED|$PERCENT_RED|g" -e "s|PERCENT_BLUE|$PERCENT_BLUE|g" $TEMPLATE > $OUTFILE
+
+    # Start experiment
+argos3 -c $OUTFILE
+    
+done
+
