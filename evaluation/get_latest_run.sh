f1f69622ecb529acb7fa4eb0ed16043b6308c686
Volker Strobel <volker.strobel87@gmail.com>
2017-12-14 18:07:00 +0100

    cleaned up

--- evaluation/get_latest_run.sh
+++
@@ -1,2 +0,0 @@
-#scp -r vstrobel@majorana.ulb.ac.be:/home/vstrobel/Documents/argdavide/volker_red* new_data/
-scp -r vstrobel@majorana.ulb.ac.be:/home/vstrobel/Documents/argdavide/volker* fullyconnected
